a4015aa21a2a389cb1c8784d27682be4b7466e01
Toru Maesaka <toru@chronohq.com>
2024-10-04 21:24:31 -0700

    radixdb: add a debug tree printer

---
+++ debug.go
@@ -0,0 +1,53 @@
+package radixdb
+
+import (
+	"fmt"
+)
+
+// DebugPrint prints the RadixDB structure in a directory tree format.
+// Do not use this function for other than development and debugging.
+func (rdb *RadixDB) DebugPrint() {
+	rdb.mu.RLock()
+	defer rdb.mu.RUnlock()
+
+	printTree(rdb.root, "", true, true, rdb.Len())
+}
+
+func printTree(node *node, prefix string, isLast bool, isRoot bool, treeSize uint64) {
+	if node == nil {
+		return
+	}
+
+	if isRoot && treeSize == 1 {
+		fmt.Printf("%s\n", string(node.key))
+		return
+	}
+
+	if isRoot {
+		if node.value != nil {
+			fmt.Printf("%s (%q)\n", string(node.key), node.value.(string))
+		} else {
+			fmt.Println(".")
+		}
+	} else {
+		var val string
+
+		if node.value != nil {
+			val = node.value.(string)
+		} else {
+			val = "<nil>"
+		}
+
+		if isLast {
+			fmt.Printf("%s└─ %s (%q)\n", prefix, string(node.key), val)
+			prefix += "  "
+		} else {
+			fmt.Printf("%s├─ %s (%q)\n", prefix, string(node.key), val)
+			prefix += "│  "
+		}
+	}
+
+	for i, child := range node.children {
+		printTree(child, prefix, i == len(node.children)-1, false, treeSize)
+	}
+}
