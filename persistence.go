4c36e85b64132bddad6cbd7a4cc68e5e41ec2bd0
Toru Maesaka <toru@chronohq.com>
2024-10-14 22:24:59 -0700

    radixdb: scaffold the file header builder

--- persistence.go
+++ persistence.go
@@ -1,10 +1,40 @@
 package radixdb
 
+import (
+	"bytes"
+	"encoding/binary"
+)
+
 const (
 	// magicByte is the first byte of a RadixDB file, used to identify the format
 	// and detect pontential corruption. It is represented by 'R' for RadixDB.
-	magicByte = 0x52
+	magicByte = byte(0x52)
 
-	// fileExt is the standard file extension for RadixDB files.
-	fileExt = ".rdx"
+	// fileFormatVersion represents the version of the database file format.
+	// It is used to ensure that the database file is compatible with the
+	// RadixDB software version.
+	fileFormatVersion = uint8(1)
 )
+
+// binaryHeaderSize returns the total size of the binary header of the database
+// file. The size is returned as an int representing the total number of bytes.
+func binaryHeaderSize() int {
+	magicByteLen := 1         // byte
+	fileFormatVersionLen := 1 // uint8
+	recordCountLen := 8       // uint64
+
+	return (magicByteLen + fileFormatVersionLen + recordCountLen)
+}
+
+// buildFileHeader builds and returns a binary header for the RadixDB database
+// file. This function encodes multi-byte native types, such as the number of
+// nodes (uint64) using little-endian encoding.
+func (rdb *RadixDB) buildFileHeader() []byte {
+	var buf bytes.Buffer
+
+	buf.WriteByte(magicByte)
+	buf.WriteByte(fileFormatVersion)
+	binary.Write(&buf, binary.LittleEndian, rdb.numNodes)
+
+	return buf.Bytes()
+}
---
+++ persistence_test.go
@@ -0,0 +1,16 @@
+package radixdb
+
+import "testing"
+
+func TestBuildFileHeader(t *testing.T) {
+	rdb := &RadixDB{}
+	rdb.Insert([]byte("apple"), []byte("sauce"))
+
+	header := rdb.buildFileHeader()
+
+	expectedSize := binaryHeaderSize()
+
+	if len := len(header); len != expectedSize {
+		t.Errorf("unexpected header size, got:%d, want:%d", len, expectedSize)
+	}
+}
